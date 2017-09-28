a1e67073186a50740ffe21de01bec895a81bb355
Rebecca <rebecca.sinkoff@gmail.com>
2017-09-28 16:50:38 +0000

    Added geocoder and figaro gems

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,6 +1,9 @@
 class Place < ApplicationRecord
     paginates_per 5
     belongs_to :user
+    geocoded_by :address
+    after_validation :geocode
+    
     validates :name, length: {minimum: 3}
     validates :address, :description, presence: true
 end
