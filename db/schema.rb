4d012979a8076dcaf9899e98d56ef65040cbe254
Rebecca <rebecca.sinkoff@gmail.com>
2017-10-03 23:01:58 +0000

    Updated migration and corrected typos

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171003144623) do
+ActiveRecord::Schema.define(version: 20171003225148) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 20171003144623) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "photo_id"
+    t.string   "picture"
   end
 
   create_table "places", force: :cascade do |t|
